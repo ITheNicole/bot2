c7afc43eff143ee4b6ce418c1f663b3701a97dfd
ITheNicole <31995790+ITheNicole@users.noreply.github.com>
2019-04-18 23:50:57 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,6 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
- client.login(process.env.NTY4NTQ0NTUzMTg4MTk2MzY4.XLjo7Q.V5t0Yy1aaDJA-eZyoLPgn6hQkkE);
 
 client.on('ready', () => {
 
@@ -10,3 +9,4 @@ client.on('ready', () => {
 
 });
 
+client.login(process.env.BOT_TOKEN);
