6d227be481c28f7a2fd3021b042b4f1a699f9d0c
ITheNicole <31995790+ITheNicole@users.noreply.github.com>
2019-04-19 12:04:22 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
         game: {
             name: 'www.evolutioncheats.xyz',
             type: "STREAMING",
-            url: "https://www.twitch.tv/"
+            url: "https://www.twitch.tv/evo"
         }
     });
 });
