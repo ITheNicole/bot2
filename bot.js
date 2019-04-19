b40c9ede0290c641ad2e3fbf900fff36b28dfca8
ITheNicole <31995790+ITheNicole@users.noreply.github.com>
2019-04-19 11:54:36 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,11 +6,11 @@ const client = new Discord.Client();
 client.on('ready', () => {
 
     console.log('I am ready!');
-
+client.user.setPresence({ game: { name: 'with depression', type: "streaming", url: "https://www.twitch.tv/monstercat"}}); 
 
 });
 
 client.login(process.env.BOT_TOKEN);
 
-bot.user.setPresence({ game: { name: 'with depression', type: "streaming", url: "https://www.twitch.tv/monstercat"}}); 
+
 
