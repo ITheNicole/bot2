12507b324f1e2a8045dada03554ac7412e318b1f
ITheNicole <31995790+ITheNicole@users.noreply.github.com>
2019-04-19 11:46:26 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,10 +6,10 @@ const client = new Discord.Client();
 client.on('ready', () => {
 
     console.log('I am ready!');
+  bot.user.setGame('GAME HERE')
+
 
 });
 
 client.login(process.env.BOT_TOKEN);
 
-bot.on('ready', () => {
-  bot.user.setGame('GAME HERE')
